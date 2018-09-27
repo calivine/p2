7fdfc2200fb9846cab6501472443dc3c4845d0f0
Alex Caloggero <a.calog@protonmail.com>
2018-09-26 21:24:06 -0400

    Update README.md; added inputs to index.php

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 *your list of outside resources go here*
 
 ## 3 Unique inputs
-*You don't have to list all inputs, just 3 unique ones. The following is an example of what I'd put if my project 
-was the xkcd Password Generator: (1) Text input to indicate number of words in password, (2) Checkbox to indicate 
-whether to include a number (3) Dropdown to indicate what symbol to include*
+(1) A number input to collect the user's remaining loan balance
+(2) Dropdown selection to choose the length of the payment cycle
+(3) A checkbox to indicate whether or not the program should display the payment schedule
 
 ## Class
 *List the name of the class you're using, e.g. Form.php, Password.php, etc.*
--- index.php
+++ index.php
@@ -13,25 +13,58 @@ require 'logic.php';
           crossorigin="anonymous">
 </head>
 <body>
-<h1>Loan Planning Assistant</h1>
-<p>This tool helps users plan loan payments. Fill out the form below and press submit.</p>
-<form method="GET" action="calculate.php">
-    <input type="number" name="principal" placeholder="Principal amount">
-    <input type="number" name="interest" step=".001" placeholder="Interest Rate">
-    <input type="number" name="payment" placeholder="Monthly Payment">
-    <input type="submit" value="Calculate">
-</form>
-<?php if (isset($int_paid)): ?>
-    <p>
-        Total Interest Paid: <?= $int_paid ?>
-    </p>
-<?php endif ?>
-<?php if (isset($payment_periods)): ?>
-    <p>
-        Payment periods: <?= $payment_periods ?>
-    </p>
-<?php endif ?>
+<div class="container">
+    <h1>Loan Map</h1>
+    <p>This tool helps users plan loan payments. Fill out the form below and press submit.</p>
+    <div class="row">
+        <section class="col-5">
+            <form method="GET" action="calculate.php">
+                <fieldset>
+                    <legend>Loan Details</legend>
+                    <label>
+                        Remaining Principal Amount
+                        <input type="number" name="principal" placeholder="$">
+                    </label>
+                    <label>
+                        Interest Rate
+                        <input type="number" name="interest" step=".001" placeholder="i.e. .001">
+                    </label>
+                    <label>
+                        Payment Amount
+                        <input type="number" name="payment" placeholder="$">
+                    </label>
+                    <label>
+                        Payment Cycle Duration
+                        <select name="paymentCycle">
+                            <option value="select">Select length</option>
+                            <option value="thirty">Thirty days</option>
+                            <option value="sixty">Sixty days</option>
+                            <option value="ninety">Ninety days</option>
+                        </select>
+                    </label>
+                    <label>
+                        Display Payment Schedule
+                        <input type='checkbox' name='display'>
+                    </label>
+                </fieldset>
 
+                <input type="submit" value="Calculate">
+            </form>
+        </section>
+        <section class="col-6 offset-1">
+            <?php if (isset($int_paid)): ?>
+                <p>
+                    Total Interest Paid: <?= $int_paid ?>
+                </p>
+            <?php endif ?>
+            <?php if (isset($payment_periods)): ?>
+                <p>
+                    Payment periods: <?= $payment_periods ?>
+                </p>
+            <?php endif ?>
+        </section>
+    </div>
+</div>
 </body>
 </html>
 
