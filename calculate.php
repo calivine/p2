fb591aa6701d06a7d9bcc1628d851d5a088045e3
Alex Caloggero <a.calog@protonmail.com>
2018-09-28 17:12:51 -0400

    Added logic for drop down select

--- calculate.php
+++ calculate.php
@@ -8,6 +8,7 @@ $principal = $_GET['principal'];
 $int_rate = $_GET['interest'];
 $payment = $_GET['payment'];
 $cycle = $_GET['paymentCycle'];
+$display = isset($_GET['display']);
 
 # calculate Interest Rate Factor
 $int_rate_factor = getInterestFactor($int_rate);
@@ -20,11 +21,25 @@ $payment_periods = 0;
 
 $paymentSchedule = [];
 
+# Get payment period term
+if($cycle == 'thirty') {
+    $paymentTerm = 30;
+}
+else if($cylce == 'sixty') {
+    $paymentTerm = 60;
+}
+else if($cycle == 'ninety') {
+    $paymentTerm = 90;
+}
+else {
+    $paymentTerm = 30; # Default if none chosen.
+}
+
 while ($principal > $payment) {
     # At the start of the cycle, calculate 30 days of interest and add to principal
     $simple_daily_int = $principal * $int_rate_factor;
     # Interest to add equals simple daily interest * days since last payment (30)
-    $int_to_add = $simple_daily_int * 30;
+    $int_to_add = $simple_daily_int * $paymentTerm;
     # Add interest to principal amount.
     $principal += $int_to_add;
     # Add interest to total interest paid
